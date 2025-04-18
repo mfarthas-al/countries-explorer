b764241c084b72f712427d11f9f9a6e63ceacb2a
mfarthas-al <almfarthas@gmail.com>
2025-04-18 19:33:24 +0530

    initial commit

---
+++ countries-explorer/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold text-blue-600">Countries Explorer 🌍</h1>
+    </div>
+  );
+}
+export default App;
